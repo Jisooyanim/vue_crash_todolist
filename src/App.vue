88e039c04d2880b3140bf7bdb8460815599e3fdf
jisooya <vladzzzzzss@gmail.com>
2023-07-05 14:37:03 +0800

    vue_tutorial

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,80 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <AddTodo v-on:add-todo="addTodo"/>
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AddTodo from './components/AddTodo'
+import Header from './components/layout/Header';
+import Todos from './components/Todos';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Header,
+    Todos,
+    AddTodo,
+  },
+  
+  //Return an object
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Learn Vue",
+          completed: true
+        },
+        {
+          id: 2,
+          title: "Had Breakfast",
+          completed: true
+        },
+        {
+          id: 3,
+          title: "Die",
+          completed: false
+        }
+      ]
+    }
+  },
+  methods: {
+    deleteTodo(id){
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    addTodo(newTodo) {
+      // ... ->spread operator, copy what is already in todos
+      this.todos = [...this.todos, newTodo];
+    },
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+
+  .btn{
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+
+  .btn-hover{
+    background: #666;
+  }
 </style>
